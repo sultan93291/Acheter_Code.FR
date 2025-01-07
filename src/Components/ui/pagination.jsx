914bcf9e61623c864feef69b4d4d901f5993b035
sultan93291 <130165694+sultan93291@users.noreply.github.com>
2025-01-07 18:43:13 +0600

    pagination issue fixed

--- src/Components/ui/pagination.jsx
+++ src/Components/ui/pagination.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
-
+import Pagination from '@/components/ui/pagination';
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button";
 
@@ -54,7 +54,7 @@ const PaginationPrevious = ({
     size="default"
     className={cn("gap-1 pl-2.5", className)}
     {...props}>
-    <ChevronLeft className="h-4 w-4" />
+    <ChevronLeft className="w-4 h-4" />
     <span>Previous</span>
   </PaginationLink>
 )
@@ -70,7 +70,7 @@ const PaginationNext = ({
     className={cn("gap-1 pr-2.5", className)}
     {...props}>
     <span>Next</span>
-    <ChevronRight className="h-4 w-4" />
+    <ChevronRight className="w-4 h-4" />
   </PaginationLink>
 )
 PaginationNext.displayName = "PaginationNext"
@@ -83,7 +83,7 @@ const PaginationEllipsis = ({
     aria-hidden
     className={cn("flex h-9 w-9 items-center justify-center", className)}
     {...props}>
-    <MoreHorizontal className="h-4 w-4" />
+    <MoreHorizontal className="w-4 h-4" />
     <span className="sr-only">More pages</span>
   </span>
 )
